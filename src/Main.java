bed93bdf904120151f44983b85a654fc68f17703
Gastech99 <diabygassamad16@gmail.com>
2025-01-12 16:39:26 +0000

    les types de lectures des données saisies par l'utilisateur

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
- //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Les types de variables en JAVA
 
         System.out.println("*******************************************");
@@ -32,5 +37,22 @@ public class Main {
         System.out.println("My Letter = "+ myLetter);
         System.out.println("My Boolean = "+ myBool);
         System.out.println("My Text = "+ myText);
+
+        System.out.println("*******************************************");
+        System.out.println("Lecture solution 1");
+
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Saisir votre chaine de caractère : ");
+        String ch = keyboard.readLine();
+        System.out.println("La valeur de ch = "+ch);
+
+        System.out.println("*******************************************");
+        System.out.println("Lecture solution 2");
+        Scanner keyboard2 = new Scanner(System.in);
+        int a;
+        System.out.println("Saisir a : ");
+        a = keyboard2.nextInt();
+        System.out.println("La valeur de a = "+a);
+        keyboard2.close();
     }
 }
