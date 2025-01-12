9a4d5144ceb383e390e4c1da6ff00cb09afc17af
Gastech99 <diabygassamad16@gmail.com>
2025-01-12 16:17:18 +0000

    Les différents types de variables

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+ //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Les types de variables en JAVA
+
+        System.out.println("*******************************************");
+        System.out.println("Les différents types de variables");
+
+        String name = "John";
+        System.out.println(name);
+
+        int myNum = 15;
+        System.out.println(myNum);
+
+        int myNum1;
+        myNum1 = 5;
+        System.out.println(myNum1);
+
+        int myNum2 = 15;
+        myNum2 = 20;  // myNum2 is now 20
+        System.out.println(myNum2);
+
+        int myNumber = 5;
+        float myFloat = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "Hello";
+
+        System.out.println("My Integer = "+ myNumber);
+        System.out.println("My Float = "+ myFloat);
+        System.out.println("My Letter = "+ myLetter);
+        System.out.println("My Boolean = "+ myBool);
+        System.out.println("My Text = "+ myText);
+    }
+}
