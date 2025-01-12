84a4d5c880b59c52593b3c9df73e2bd17a62acaf
Gastech99 <diabygassamad16@gmail.com>
2025-01-12 22:27:12 +0000

    Utilisation de la boucle for

---
+++ src/Boucles.java
@@ -0,0 +1,9 @@
+public class Boucles {
+    public static void main(String[] args){
+        System.out.println("*************************************");
+        System.out.println("Résultat du boucle for");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
